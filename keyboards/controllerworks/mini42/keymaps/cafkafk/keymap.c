f564a671dd5d327a0678a2ef5f202e0774dad4ea
Christina Sørensen <christina@cafkafk.com>
2023-07-27 12:04:06 +0200

    fix: previous commit was super annoying
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- keyboards/controllerworks/mini42/keymaps/cafkafk/keymap.c
+++ keyboards/controllerworks/mini42/keymaps/cafkafk/keymap.c
@@ -47,7 +47,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_split_3x6_3(
                                                                   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-                                                                  LT(4, KC_TAB), LCTL_T(KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, LALT_T(KC_P), KC_BSLS,
+                                                                  LT(4, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, LCTL_T(KC_BSLS),
                                                                   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
                                                                   LGUI_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, RGUI_T(KC_QUOT),
                                                                   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
